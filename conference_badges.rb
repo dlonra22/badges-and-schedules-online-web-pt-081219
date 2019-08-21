117c563c2be788b817f344eeb4c9209dd9dd04ff
arnie b <arnold.bwaila@gmail.com>
2019-08-21 21:32:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker(name)
+  message = "Hello, my name is #{name}."
+  message
+end
+
+def batch_badge_creator(array)
+   message = []
+   array.each do |elmnt|
+     message << "Hello, my name is #{elmnt}."
+   end
+   message
+ end
+
+def assign_rooms(array)
+    message[]
+    cnt = 1 
+    array.each do |elmnt|
+      message << "Hello, #{elmnt}! You'll be assigned to room #{cnt}!"]
+      cnt+= 1 
+    end
+  end
+    
